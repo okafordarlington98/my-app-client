a536e929218f37185ba9511fc797eaedab17c2a3
okafordarlington98 <281412446+okafordarlington98@users.noreply.github.com>
2026-06-11 09:34:56 +0200

    implementing functionalities

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+
+            <div>
+                <label htmlFor="">Search Projects!</label>
+                <input type="text" />
+            </div>
+
+        </div>
+    );
+}
