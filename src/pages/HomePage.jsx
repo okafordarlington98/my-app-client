8f9eff302e58167f831acc7199711f9e179c7a7f
okafordarlington98 <281412446+okafordarlington98@users.noreply.github.com>
2026-06-11 09:38:26 +0200

    search done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ function HomePage() {
             <h1>Home Page</h1>
 
             <div>
+                <h3>functionality up and running! all done</h3>
                 <label htmlFor="">Search Projects!</label>
                 <input type="text" />
             </div>
